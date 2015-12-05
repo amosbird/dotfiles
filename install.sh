c339e228083dd1e7845cec5c283a0fabb7ebc6a2
Amos Bird <amosbird@gmail.com>
2015-12-05 22:22:03 +0800

    Add basic X functionality

---
+++ install.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Source: https://gist.github.com/davejamesmiller/1965569
+ask() {
+  while true; do
+    if [ "${2:-}" = "Y" ]; then
+      prompt="Y/n"
+      default=Y
+    elif [ "${2:-}" = "N" ]; then
+      prompt="y/N"
+      default=N
+    else
+      prompt="y/n"
+      default=
+    fi
+    read -p "$1 [$prompt] " REPLY </dev/tty
+    if [ -z "$REPLY" ]; then
+      REPLY=$default
+    fi
+    case "$REPLY" in
+      Y*|y*) return 0 ;;
+      N*|n*) return 1 ;;
+    esac
+  done
+}
+
+dir=`pwd`
+
+if ask "Install symlink for .spacemacs" Y; then
+  ln -sfn ${dir}/.spacemacs ${HOME}/.spacemacs
+fi
+
+if ask "Install symlink for .pentadactyl" Y; then
+  ln -sfn ${dir}/.pentadactyl ${HOME}/.pentadactyl
+fi
+
+if ask "Install symlink for .Xresources?" Y; then
+  ln -sf ${dir}/.Xresources ${HOME}/.Xresources
+fi
+
+if ask "Install symlink for .xinitrc?" Y; then
+  ln -sf ${dir}/.xinitrc ${HOME}/.xinitrc
+fi
+
+if ask "Install symlink for .i3blocks.conf?" Y; then
+  ln -sf ${dir}/.i3blocks.conf ${HOME}/.i3blocks.conf
+fi
+
+if ask "Install symlink for .scripts?" Y; then
+  ln -sfn ${dir}/.scripts ${HOME}/
+fi
+
+if ask "Install symlink for .screenlayout?" Y; then
+  ln -sfn ${dir}/.screenlayout ${HOME}/
+fi
+
+if ask "Install symlink for .i3?" Y; then
+  ln -sfn ${dir}/.i3 ${HOME}/
+fi
+
+if ask "Install symlink for .config" Y; then
+  ln -sfn ${dir}/.config ${HOME}/
+fi
+
+if ask "Install vim config?" Y; then
+  mkdir -p ${HOME}/.vim/autoload
+  cp ${dir}/plugged.vim ${HOME}/.vim/autoload
+  ln -sfn ${dir}/.vimrc ${HOME}/.vimrc
+fi
